5fa2b5d2a9ebd488b82a937e65d9466cfb4abe00
Ramon <R.amanifard@gmail.com>
2025-01-12 22:42:30 +0200

    C 15

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramanifa <ramanifa@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 19:53:55 by ramanifa          #+#    #+#             */
+/*   Updated: 2024/09/14 20:41:55 by ramanifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i +1;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	d[50];
+	char	s[] = "wellcome to helllllllllllll";
+	ft_strncpy(d, s, 20);
+	printf("My string is: %s\n", d);
+	return(0);
+}
+*/
