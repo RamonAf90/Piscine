21bf61d9510c6f01ba080cf45b4451877cc68614
Ramon <R.amanifard@gmail.com>
2025-01-12 22:43:44 +0200

    C 19

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramanifa <ramanifa@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 14:49:34 by ramanifa          #+#    #+#             */
+/*   Updated: 2024/09/15 14:53:37 by ramanifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	if (*str == '\0')
+		return (1);
+	while (*str != '\0')
+	{
+		if (*str < 65 || *str > 90)
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+	char	str1[] = "HELLO";
+	char	str2[] = "hello";
+	char	str3[] = "";
+	char	str4[] = "gG21F";
+
+	printf("%d\n", ft_str_is_uppercase(str1));
+	printf("%d\n", ft_str_is_uppercase(str2));
+	printf("%d\n", ft_str_is_uppercase(str3));
+	printf("%d\n", ft_str_is_uppercase(str4));
+	return(0);
+}
+*/
