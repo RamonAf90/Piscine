aba21b49e9bea66e29936b18089a2a8062fa7652
Ramon <R.amanifard@gmail.com>
2025-01-12 22:28:27 +0200

    C 6

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramanifa <ramanifa@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 19:11:42 by ramanifa          #+#    #+#             */
+/*   Updated: 2024/09/12 10:00:37 by ramanifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*
+int	main(void)
+{
+	int	a;
+
+	a = 20;
+	printf("address of a: %p\n", &a);
+	printf("value of a: %d\n", a);
+	ft_ft(&a);
+	printf("address of a: %p\n", &a);
+	printf("value of a: %d\n", a);
+	return(0);
+}
+*/
