50df8a94a5bc863a2d3144e2716730f6e51499e2
Ramon <R.amanifard@gmail.com>
2025-01-12 22:29:25 +0200

    C 7

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramanifa <ramanifa@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 13:10:22 by ramanifa          #+#    #+#             */
+/*   Updated: 2024/09/12 10:09:17 by ramanifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main(void)
+{
+	int	a;
+	int	*b;
+	int	**c;
+	int	***d;
+	int	****e;
+	int	*****f;
+	int	******g;
+	int	*******h;
+	int	********i;
+
+	a = 2;
+	b = &a;
+	c = &b;
+	d = &c;
+	e = &d;
+	f = &e;
+	g = &f;
+	h = &g;
+	i = &h;
+	printf("%d\n", a);
+	ft_ultimate_ft(&i);
+	printf("%d\n", a);
+	return (0);
+}
+*/
